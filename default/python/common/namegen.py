66f03ba08abbe89489a3bd477023a43c5be2cdc1
Vezzra <vezzra@gmx.net>
2016-09-01 20:26:12 +0200

    First draft of random name generator

---
+++ default/python/common/namegen.py
@@ -0,0 +1,155 @@
+import sys
+from random import choice, random, randint
+
+
+class SingleLetter(object):
+    def __init__(self, letter):
+        self.__letter__ = letter
+
+    def __call__(self, *args, **kwargs):
+        return self.__letter__
+
+
+class LetterGroup(SingleLetter):
+    def __init__(self, *args):
+        self.letters = list(args)
+
+    def __call__(self, *args, **kwargs):
+        exclude = kwargs.get("exclude", [])
+        while True:
+            letter = choice(self.letters)()
+            if letter not in exclude:
+                return letter
+
+
+lg_a = LetterGroup(
+        SingleLetter("a"),
+        SingleLetter("a"),
+        SingleLetter("a"),
+        SingleLetter("a"),
+        SingleLetter("ai"),
+        SingleLetter("au")
+)
+
+lg_e = LetterGroup(
+        SingleLetter("e"),
+        SingleLetter("e"),
+        SingleLetter("e"),
+        SingleLetter("e"),
+        SingleLetter("ei"),
+        SingleLetter("eu")
+)
+
+lg_i = LetterGroup(
+        SingleLetter("i"),
+        SingleLetter("i"),
+        SingleLetter("ie")
+)
+
+lg_o = LetterGroup(
+        SingleLetter("o"),
+        SingleLetter("o"),
+        SingleLetter("ou")
+)
+
+vowels = LetterGroup(
+    lg_a,
+    lg_a,
+    lg_e,
+    lg_e,
+    lg_i,
+    lg_i,
+    lg_o,
+    lg_o,
+    SingleLetter("u"),
+    SingleLetter("u"),
+    SingleLetter("y")
+)
+
+consonants = LetterGroup(
+    SingleLetter("b"),
+    LetterGroup(
+        SingleLetter("c"),
+        SingleLetter("c"),
+        SingleLetter("c"),
+        SingleLetter("c"),
+        SingleLetter("ch"),
+        SingleLetter("ck")
+    ),
+    SingleLetter("d"),
+    SingleLetter("f"),
+    SingleLetter("g"),
+    SingleLetter("h"),
+    SingleLetter("j"),
+    SingleLetter("k"),
+    SingleLetter("l"),
+    SingleLetter("m"),
+    SingleLetter("n"),
+    SingleLetter("p"),
+    LetterGroup(
+        SingleLetter("qu"),
+        SingleLetter("qu"),
+        SingleLetter("q")
+    ),
+    SingleLetter("r"),
+    LetterGroup(
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("s"),
+        SingleLetter("sch"),
+        SingleLetter("sh"),
+        SingleLetter("sh"),
+        SingleLetter("sp"),
+        SingleLetter("sp"),
+        SingleLetter("st"),
+        SingleLetter("st")
+    ),
+    SingleLetter("t"),
+    SingleLetter("v"),
+    SingleLetter("w"),
+    SingleLetter("x"),
+    SingleLetter("z")
+)
+
+
+def get_syllable(start=False, end=False, dont_start_with=list()):
+    retval = []
+    if (start and not end) or (not start and end):
+        retval.append(consonants(exclude=dont_start_with) if random() < 0.6 else vowels(exclude=dont_start_with))
+    else:
+        if random() < 0.6:
+            letter = consonants(exclude=dont_start_with)
+            retval.append(letter)
+            letter = vowels(exlude=[letter])
+            retval.append(letter)
+            letter = consonants(exlude=[letter]+["qu", "q"])
+            retval.append(letter)
+        else:
+            letter = vowels(exclude=dont_start_with)
+            retval.append(letter)
+            letter = consonants(exlude=[letter])
+            retval.append(letter)
+            letter = vowels(exlude=[letter])
+            retval.append(letter)
+    return retval
+
+
+names = []
+
+for _ in range(100):
+    middle_syllables = []
+    first_letter = get_syllable(start=True)
+    last_letter = first_letter
+    for _ in range(randint(1, 2)):
+        syl = get_syllable(dont_start_with=[last_letter])
+        middle_syllables.extend(syl)
+        last_letter = syl[-1]
+    last_letter = get_syllable(end=True, dont_start_with=[last_letter] + ["qu"])
+    names.append("".join(first_letter + middle_syllables + last_letter).capitalize())
+
+names.sort()
+print "\n".join(names)
